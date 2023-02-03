37de2fa22142c9970d8b2460bae31d89c69ab348
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-02-03 01:09:49 +0100

    geolocation function using ipgeolocation api

--- weathermusic/main/localization.py
+++ weathermusic/main/localization.py
@@ -1,9 +1,12 @@
 from requests import get
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 API_KEY = os.getenv('GEOLOCATION_KEY')
+
+# This function returns user ip address
 
 
 def get_ip(request):
@@ -14,3 +17,17 @@ def get_ip(request):
         ip = request.META.get('REMOTE_ADDR')
 
     return ip
+
+# This function return user geolocation based on ip address
+
+
+def geolocation(ip_address):
+    base_url = f'https://ipgeolocation.abstractapi.com/v1/?api_key={API_KEY}'
+    result = get(base_url)
+    json_result = json.loads(result.content)
+
+    if result.status_code == 200:
+        city_name = json_result["city"]
+        return city_name
+    else:
+        print("Nie działa")
