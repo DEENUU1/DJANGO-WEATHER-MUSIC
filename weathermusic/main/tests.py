79849b48716af5e5466eda40ca372216e860ce83
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-02-11 22:54:34 +0100

    Test for spotify application

--- weathermusic/main/tests.py
+++ weathermusic/main/tests.py
@@ -3,11 +3,12 @@ from django.urls import resolve, reverse
 from .views import main_view
 from unittest.mock import MagicMock, patch
 import datetime
+from .spotify import SpotifyCategory
 
 
 class TestUrls(SimpleTestCase):
 
-    def test_main_resolve(self):
+    def test_main_url_resolve(self):
         url = reverse('weather_music:main')
         self.assertEqual(resolve(url).func, main_view)
 
@@ -20,7 +21,9 @@ class MainViewTests(TestCase):
     @patch('main.views.News')
     @patch('main.views.datetime')
     def test_main_view(self, mock_datetime, mock_news, mock_playlist_info, mock_weather_info, mock_city_name):
+        # mock to return city name
         mock_city_name.return_value = 'London'
+        # mock to return weather data
         mock_weather_info.return_value = MagicMock(
             temp=10,
             desc='cloudy',
@@ -30,11 +33,13 @@ class MainViewTests(TestCase):
             min_temp=5,
             wind_speed=5
         )
+        # mock to return spotify playlist info
         mock_playlist_info.return_value = {
             'playlist_title': 'London Cloudy Playlist',
             'playlist_url': 'https://londoncloudy.com',
             'playlist_image': 'cloudy.jpg'
         }
+        # mock to return news data
         mock_news.return_value = MagicMock(
             get_news=MagicMock(return_value=[{
                 'title': 'News article 1',
@@ -46,10 +51,35 @@ class MainViewTests(TestCase):
                 'img_url': 'news2.jpg'
             }])
         )
+        # mock to return user local time
         mock_datetime.now.return_value = datetime.datetime(2023, 2, 11, 11, 0)
 
         response = self.client.get(reverse('weather_music:main'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
         self.assertEqual(response.context['playlist_title'], 'London Cloudy Playlist')
+
+
+class TestSpotify(TestCase):
+
+    def setUp(self) -> None:
+        self.token = "dsadasdqeasjasidjasidjasijdasidjas"
+        self.playlist_id = "dsasdasdas9di9asid9as"
+        self.playlist_title = "Wiosenne wibracje"
+        self.playlist_url = "spotify.com/asdasdasdad/d"
+        self.playlist_image = "dasd.jpg"
+        self.spotify = SpotifyCategory()
+
+    @patch('main.spotify.SpotifyCategory._search_playlist')
+    def test_playlist_search_playlist(self, mock_search_playlist):
+        mock_search_playlist.return_value = (
+            self.playlist_title, self.playlist_url, self.playlist_image)
+
+        playlist_title, playlist_url, playlist_image = self.spotify._search_playlist(
+            self.token, self.playlist_id)
+
+        self.assertEqual(playlist_title, 'Wiosenne wibracje')
+        self.assertEqual(playlist_url, 'spotify.com/asdasdasdad/d')
+        self.assertEqual(playlist_image, 'dasd.jpg')
+
 
