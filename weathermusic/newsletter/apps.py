09a3e8e7ed51bf84955b2f707a505e85309158ee
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-02-12 22:35:18 +0100

    I moved function to send welcome email into signals.py. Now when new user is registered email is automatically send

--- weathermusic/newsletter/apps.py
+++ weathermusic/newsletter/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class NewsletterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'newsletter'
+
+    def ready(self):
+        import newsletter.signals
---
+++ weathermusic/newsletter/signals.py
@@ -0,0 +1,26 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.conf import settings
+from .models import UserInfo
+
+
+@receiver(post_save, sender=UserInfo)
+def create_user_info(sender, instance, created, **kwargs):
+    if created:
+        template = render_to_string('newsletter_welcome.html',
+                                    {'name':instance.name})
+
+        subject_email = 'Dziękujemy za rejestrację do newslettera.'
+
+        email = EmailMessage(
+            subject_email,
+            template,
+            settings.EMAIL_HOST_USER,
+            [instance.email],
+        )
+
+        email.fail_silently = False
+        email.send()
+
