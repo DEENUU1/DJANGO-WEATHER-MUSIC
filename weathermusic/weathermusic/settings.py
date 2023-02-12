80d80f5d6fc67b1914006a7167200167b265164f
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-02-12 02:43:47 +0100

    news application

--- weathermusic/weathermusic/settings.py
+++ weathermusic/weathermusic/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'newsletter',
+    'news',
     'crispy_forms',
     'tinymce',
 
--- weathermusic/weathermusic/urls.py
+++ weathermusic/weathermusic/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('newsletter/', include('newsletter.urls'))
+    path('newsletter/', include('newsletter.urls')),
+    path('news/', include('news.urls')),
 ]
