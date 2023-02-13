9ad96b479d51443a67e54e457db417d68cb4c648
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-02-13 23:09:59 +0100

    Configuration and implementation of static files

--- weathermusic/weathermusic/settings.py
+++ weathermusic/weathermusic/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 ''' EMAIL '''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
