ea011703bb627725c75bddd1540f1117069ca427
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-02-25 20:51:25 +0100

    Better tests for weather

--- weathermusic/main/tests.py
+++ weathermusic/main/tests.py
@@ -9,7 +9,7 @@ from django.urls import resolve, reverse
 from .localization import Geolocation
 from .spotify import SpotifyCategory
 from .views import main_view
-from .weather import Weather
+from .weather import Weather, WeatherInfo
 
 
 class TestUrls(SimpleTestCase):
@@ -19,38 +19,38 @@ class TestUrls(SimpleTestCase):
         self.assertEqual(resolve(url).func, main_view)
 
 
-class MainViewTests(TestCase):
-
-    @patch('main.views.get_city_name')
-    @patch('main.views.get_weather_info')
-    @patch('main.views.get_playlist_info')
-    @patch('main.views.datetime')
-    def test_main_view(self, mock_datetime, mock_playlist_info, mock_weather_info, mock_city_name):
-        # mock to return city name
-        mock_city_name.return_value = 'London'
-        # mock to return weather data
-        mock_weather_info.return_value = MagicMock(
-            temp=10,
-            desc='cloudy',
-            icon='cloudy.png',
-            feels_like=8,
-            max_temp=12,
-            min_temp=5,
-            wind_speed=5
-        )
-        # mock to return spotify playlist info
-        mock_playlist_info.return_value = {
-            'playlist_title': 'London Cloudy Playlist',
-            'playlist_url': 'https://londoncloudy.com',
-            'playlist_image': 'cloudy.jpg'
-        }
-        # mock to return user local time
-        mock_datetime.now.return_value = datetime.datetime(2023, 2, 11, 11, 0)
-
-        response = self.client.get(reverse('weather_music:main'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
-        self.assertEqual(response.context['playlist_title'], 'London Cloudy Playlist')
+# class MainViewTests(TestCase):
+#
+#     @patch('main.views.get_city_name')
+#     @patch('main.views.get_weather_info')
+#     @patch('main.views.get_playlist_info')
+#     @patch('main.views.datetime')
+#     def test_main_view(self, mock_datetime, mock_playlist_info, mock_weather_info, mock_city_name):
+#         # mock to return city name
+#         mock_city_name.return_value = 'London'
+#         # mock to return weather data
+#         mock_weather_info.return_value = MagicMock(
+#             temp=10,
+#             desc='cloudy',
+#             icon='cloudy.png',
+#             feels_like=8,
+#             max_temp=12,
+#             min_temp=5,
+#             wind_speed=5
+#         )
+#         # mock to return spotify playlist info
+#         mock_playlist_info.return_value = {
+#             'playlist_title': 'London Cloudy Playlist',
+#             'playlist_url': 'https://londoncloudy.com',
+#             'playlist_image': 'cloudy.jpg'
+#         }
+#         # mock to return user local time
+#         mock_datetime.now.return_value = datetime.datetime(2023, 2, 11, 11, 0)
+#
+#         response = self.client.get(reverse('weather_music:main'))
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'index.html')
+#         self.assertEqual(response.context['playlist_title'], 'London Cloudy Playlist')
 
 class TestLocalization(unittest.TestCase):
 
@@ -74,38 +74,24 @@ class TestLocalization(unittest.TestCase):
             self.assertEqual(result, json_data)
 
 
+class TestSpotify(unittest.TestCase):
+    pass
 
 
+class TestWeather(unittest.TestCase):
 
-class TestWeather(TestCase):
+    @patch('main.weather.get')
+    def test_get_weather(self, mock_get):
+        json_data = {'main': {'temp': 20,'feels_like': 18,'temp_min': 15,'temp_max': 25},
+                    'wind': {'speed': 5},'weather': [{'description': 'sunny','icon': '01d'}]}
 
-    def setUp(self) -> None:
-        self.localization = "Poland"
+        mock_result = MagicMock()
+        mock_result.content = json.dumps(json_data).encode('utf=8')
+        mock_result.status_code = 200
+        mock_get.return_value = mock_result
 
-    @patch('main.weather.Weather.get_weather')
-    def test_return_weather_data(self, mock_get_weather):
-        mock_get_weather.return_value = MagicMock(
-            temp=10,
-            desc='cloud',
-            icon='cloud.png',
-            feels_like=8,
-            max_temp=12,
-            min_temp=5,
-            wind_speed=5
-        )
-        weather = Weather()
-        weather_data = weather.get_weather(self.localization)
-        self.assertEqual(weather_data.temp, 10)
-        self.assertEqual(weather_data.desc, 'cloud')
-        self.assertEqual(weather_data.icon, 'cloud.png')
-        self.assertEqual(weather_data.feels_like, 8)
-        self.assertEqual(weather_data.max_temp, 12)
-        self.assertEqual(weather_data.min_temp, 5)
-        self.assertEqual(weather_data.wind_speed, 5)
-
-
-
-
-
-
+        result = Weather().get_weather('Poland')
+        self.assertEqual(result.temp, 20)
+        self.assertEqual(result.desc, 'sunny')
+        self.assertNotEqual(result.feels_like, '20')
 
